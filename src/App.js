eee15fbf1fb379c57f4557ba9f16f3f9244ae7ed
Gareth Bk <garethbk@gmail.com>
2018-07-30 22:00:37 -0700

    Project setup and first videos

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider, Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import logo from './logo.svg';
+import './App.css';
+
+const client = new ApolloClient({
+  uri: 'https://api-uswest.graphcms.com/v1/cjk962bvc16tf01d2rl12ial4/master',
+});
+
+const POSTS_QUERY = gql`
+  {
+    posts {
+      id
+      title
+      body
+    }
+  }
+`;
+
+// client
+//   .query({
+//     query: testQuery,
+//   })
+//   .then(res => console.log(res));
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <Query query={POSTS_QUERY}>
+            {({ data, loading }) => {
+              if (loading) {
+                return 'Loading...';
+              }
+
+              const { posts } = data;
+
+              return posts.map(post => <h1>{post.title}</h1>);
+            }}
+          </Query>
+        </div>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
