987819253e5acc3bbf45548be9e60a58349b2042
Gareth Bk <garethbk@gmail.com>
2018-08-08 08:08:48 -0700

    Video 19 finished

--- src/Posts/Posts.js
+++ src/Posts/Posts.js
@@ -10,29 +10,52 @@ export default class Posts extends Component {
         <Link className="button" to="/post/new">
           New Post
         </Link>
-        <ul className="posts-listing">
+        <ol className="posts-listing">
           <Query query={POSTS_QUERY}>
-            {({ data, loading }) => {
+            {({ data, fetchMore, loading }) => {
               if (loading) return 'Loading...';
 
               const { posts } = data;
 
-              return posts.map(post => (
-                <li key={post.id}>
-                  <Link to={`/post/${post.id}`}>{post.title}</Link>
-                </li>
-              ));
+              return (
+                <React.Fragment>
+                  {posts.map(post => (
+                    <li key={post.id}>
+                      <Link to={`/post/${post.id}`}>{post.title}</Link>
+                    </li>
+                  ))}
+                  <li>
+                    <button
+                      onClick={() =>
+                        fetchMore({
+                          variables: {
+                            skip: posts.length,
+                          },
+                          updateQuery: (prev, { fetchMoreResult }) => {
+                            if (!fetchMoreResult) return prev;
+                            return Object.assign({}, prev, {
+                              posts: [...prev.posts, ...fetchMoreResult.posts],
+                            });
+                          },
+                        })
+                      }
+                    >
+                      Load More
+                    </button>
+                  </li>
+                </React.Fragment>
+              );
             }}
           </Query>
-        </ul>
+        </ol>
       </div>
     );
   }
 }
 
 const POSTS_QUERY = gql`
-  query allPosts {
-    posts {
+  query allPosts($skip: Int) {
+    posts(orderBy: createdAt_DESC, first: 10, skip: $skip) {
       id
       title
       body
